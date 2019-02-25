cd9081c0bc145f4d7b18fb50ab6e6af052178e93
Mike Liao <mike.hm.liao@gmail.com>
2019-02-25 23:08:09 +0800

    init first 12 cards to play and build a func to deal cards

--- set/SetGame.swift
+++ set/SetGame.swift
@@ -11,28 +11,58 @@ import Foundation
 struct SetGame {
     
     private(set) var cards = [Card]()
-    private var choosenCards = [Card]()
+    private(set) var chosenCards = [Card](), matchedCards = [Card](), playedCards = [Card]()
     
     mutating func chooseCard(at index: Int) {
-        choosenCards.append(cards[index])
-        if choosenCards.count == 3 {
-            checkSet(of: choosenCards)
-            choosenCards = []
+        chosenCards.append(cards[index])
+        if chosenCards.count == 3 {
+            if checkSet(of: chosenCards) {
+                matchedCards += chosenCards
+            }
+            chosenCards = []
         }
     }
     
-    mutating func checkSet(of cards: [Card]) {
-    //  IDEA: use Set(Array) to check whether arrays formed from card's four attribute have all equal elements or all unqiue elements. Set(Array).count == 1 or Set(Array).count == 3
+    private mutating func checkSet(of chosenCards: [Card]) -> Bool {    //  IDEA: use Set(Array) to check whether arrays formed from card's four attribute have all equal elements or all unqiue elements. Set(Array).count == 1 or Set(Array).count == 3
+        let colorAttribute = chosenCards.map { $0.cardAttribute.color }
+        let numberAttribute = chosenCards.map { $0.cardAttribute.number }
+        let shadeAttribute = chosenCards.map { $0.cardAttribute.shade }
+        let shapeAttribute = chosenCards.map { $0.cardAttribute.shape }
+        if Set(colorAttribute).setFormed &&
+            Set(numberAttribute).setFormed &&
+            Set(shadeAttribute).setFormed &&
+            Set(shapeAttribute).setFormed {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    mutating func dealCards(with numberOfCards: Int) {
+        playedCards = Array(cards[0...numberOfCards-1])
+        cards = Array(cards[numberOfCards...])
+        
     }
     
     init() {
-//        initiate 81 cards in the set game
+        // initiate 81 cards in the set game
         for _ in 1...81 {
             let card = Card()
             cards += [card]
         }
         cards = cards.shuffled()
+        dealCards(with: 12)
     }
     
 }
 
+// extend protocol
+extension Collection {
+    var setFormed: Bool {
+        if count == 1 || count == 3 {
+            return true
+        } else {
+            return false
+        }
+    }
+}
--- set/ViewController.swift
+++ set/ViewController.swift
@@ -22,7 +22,10 @@ class ViewController: UIViewController {
     }
     
     private func updateCardIdentifier() {
-        var game = SetGame()
+        let game = SetGame()
+        
+        print("initial cards: \(game.playedCards.count)")
+        print("remaining cards: \(game.cards.count)")
 //        var caseText: String
 //        switch game[1].cardNumber {
 //        case .choiceOne:
@@ -34,9 +37,17 @@ class ViewController: UIViewController {
 //        }
 //
 //        cardIdentifier.text = caseText
-        print("first card: \(game.cards[0])")
-        let attribute = game.cards[0].cardAttribute
-        print("first card attribute: \(Set([attribute.color, attribute.number, attribute.shape, attribute.shade]))")
+//        print("first card: \(game.cards[0])")
+//        let attribute = game.cards[0].cardAttribute
+//        print("first card attribute: \(Set([attribute.color, attribute.number, attribute.shape, attribute.shade]))")
+//        print("first 3 cards: \(game.cards[0...2])")
+//        let setCards = game.cards[0...2]
+//
+//
+//        print("color array: \(setCards.map { $0.cardAttribute.color })")
+//        let resultAll = game.checkSet(of: Array(setCards))
+//        print("results: \(resultAll)")
+//
 //        print("set testing: \(Set(game.cards[0].cardAttribute))")
         
 //        print("cases: \(cases)")
