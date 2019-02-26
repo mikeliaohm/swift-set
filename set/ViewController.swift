22ee96789401d91aeff0f41a4daa486cd2f2884b
Mike Liao <mike.hm.liao@gmail.com>
2019-02-26 23:27:31 +0800

    started building UI

--- set/ViewController.swift
+++ set/ViewController.swift
@@ -15,59 +15,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBOutlet weak var cardIdentifier: UILabel! {
-        didSet {
-            updateCardIdentifier()
+    private lazy var game = SetGame()
+    
+    @IBOutlet var cardButtons: [UIButton]!
+
+    @IBAction func chooseCard(_ sender: UIButton) {
+        updateViewFromModel()
+    }
+    
+    private func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.playedCards[index]
+
+            button.backgroundColor = (card != nil) ? #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0)
         }
     }
     
-    private func updateCardIdentifier() {
-        var game = SetGame()
-        
-        print("initial cards: \(game.playedCards.count)")
-        print("remaining cards: \(game.cards.count)")
-//        var caseText: String
-//        switch game[1].cardNumber {
-//        case .choiceOne:
-//            caseText = "one"
-//        case .choiceTwo:
-//            caseText = "two"
-//        case .choiceThree:
-//            caseText = "three"
-//        }
-//
-        let space =  [1, nil, 2, nil].firstIndex(of: nil)
-        print("\(space!)")
-        print("played cards: \(game.playedCards)")
-//        cardIdentifier.text = caseText
-//        print("first card: \(game.cards[0])")
-//        let attribute = game.cards[0].cardAttribute
-//        print("first card attribute: \(Set([attribute.color, attribute.number, attribute.shape, attribute.shade]))")
-//        print("first 3 cards: \(game.cards[0...2])")
-//        var setCards = game.cards[0...2]
-//
-//
-//        print("color array: \(setCards.map { $0.cardAttribute.color })")
-//        let resultAll = game.checkSet(of: Array(setCards))
-//        print("results: \(resultAll)")
-
-//        print("set testing: \(Set(game.cards[0].cardAttribute))")
-        
-//        print("cases: \(cases)")
-//        print("cases: \(cases[80][0])")
-//        print("case count: \(cases.count)")
-//        let attribute = CardAttribute.color(.choiceThree)
-//        let attribute1 = cases[80][0]
-//        let result = attribute1 == attribute
-//        print("\(result)")
-//        var attribute3: CardAttribute
-//        attribute3(.color) = attribute1
-//        let attribute2 = CardAttribute(.color(.choiceOne), .number(.choiceOne), .shade(.choiceOne), .shape(.choiceOne))
-//        print("\(attribute2)")
-    
-
-    }
-    
 }
+
 
 
