8a4e681c74bd8345ac7dd98ef9096f68068fc078
Mike Liao <mike.hm.liao@gmail.com>
2019-03-03 22:36:58 +0900

    use NSAttributeString.Key to display the card shading

--- set/ViewController.swift
+++ set/ViewController.swift
@@ -37,39 +37,59 @@ class ViewController: UIViewController {
     }
     
     private func updateViewFromModel() {
+        var displayShape: String
+        var displayFill: Float
+        var displayShade = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        var displayColor: UIColor
+        
         for index in cardButtons.indices {
             let button = cardButtons[index]
             let card = game.playedCards[index]
 
             switch card?.cardAttribute.color {
             case .choiceOne?:
-                    button.backgroundColor = cardColor[0]
+                displayColor = cardColor[0]
+//                    button.backgroundColor = cardColor[0]
             case .choiceTwo?:
-                    button.backgroundColor = cardColor[1]
+                displayColor = cardColor[1]
+//                    button.backgroundColor = cardColor[1]
             case .choiceThree?:
-                    button.backgroundColor = cardColor[2]
+                displayColor = cardColor[2]
+//                    button.backgroundColor = cardColor[2]
             case .none:
-                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+                displayColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
             }
             
             switch card?.cardAttribute.shape {
-//                var buttonShape: String?
-                case .choiceOne?:
-//                buttonShape = cardShape[0]
-                button.setTitle(cardShape[0], for: UIControl.State.normal)
-                case .choiceTwo?:
-                button.setTitle(cardShape[1], for: UIControl.State.normal)
-                case .choiceThree?:
-                button.setTitle(cardShape[2], for: UIControl.State.normal)
-                default:
-                button.setTitle("", for: UIControl.State.normal)
+            case .choiceOne?:
+                displayShape = cardShape[0]
+            case .choiceTwo?:
+                displayShape = cardShape[1]
+            case .choiceThree?:
+                displayShape = cardShape[2]
+            default:
+                displayShape = ""
+            }
+            
+            switch card?.cardAttribute.shade {
+            case .choiceOne?:
+                displayFill = -1.0
+                displayShade = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.15)
+            case .choiceTwo?:
+                displayFill = -1.0
+            case .choiceThree?:
+                displayFill = 5.0
+            default:
+                displayFill = 5.0
             }
             
             let attributes: [NSAttributedString.Key:Any] = [
-                .strokeWidth : 5.0,
-                .strokeColor : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                .strokeColor : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
+                .strokeWidth : displayFill,
+                .foregroundColor: displayShade
+                ,
             ]
-            let attributedString = NSAttributedString(string: "tests", attributes: attributes)
+            let attributedString = NSAttributedString(string: displayShape, attributes: attributes)
             button.setAttributedTitle(attributedString, for: UIControl.State.normal)
 //            button.backgroundColor = (card != nil) ? #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0)
         }
