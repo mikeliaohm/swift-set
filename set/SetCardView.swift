a7a9d0e4999e3e03411e37d1ef1629b8175b1d6e
Mike Liao (mac mini) <mike.hm.liao@gmail.com>
2019-03-26 23:09:45 +0800

    imported Grid.swift

---
+++ set/SetCardView.swift
@@ -0,0 +1,21 @@
+//
+//  SetCardView.swift
+//  set
+//
+//  Created by Hsin-Min Liao on 2019/3/26.
+//  Copyright © 2019 Hsin-Min Liao. All rights reserved.
+//
+
+import UIKit
+
+class SetCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
