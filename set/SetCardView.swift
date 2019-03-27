e96b26388fea12de31ae6e0d249876ef1feac53a
Mike Liao (mac mini) <mike.hm.liao@gmail.com>
2019-03-28 00:14:31 +0800

    working on UIBezierPath

--- set/SetCardView.swift
+++ set/SetCardView.swift
@@ -21,18 +21,34 @@ class SetCardView: UIView {
     lazy var gridFrame = bounds
     
     override func draw(_ rect: CGRect) {
-        let grid = Grid(layout: Grid.Layout.dimensions(rowCount: 5, columnCount: 5), frame: gridFrame)
-        
-        let roundedRectOne = UIBezierPath(roundedRect: grid[1, 1]!, cornerRadius: 0.0)
-        let roundedRectTwo = UIBezierPath(roundedRect: grid[1, 2]!, cornerRadius: 0.0)
-        let roundedRectThree = UIBezierPath(roundedRect: grid[0, 0]!, cornerRadius: 0.0)
-        let roundedRectFour = UIBezierPath(roundedRect: grid[4, 4]!, cornerRadius: 0.0)
-        let roundedRectFive = UIBezierPath(roundedRect: grid[3, 0]!, cornerRadius: 0.0)
+        let grid = Grid(layout: Grid.Layout.dimensions(rowCount: 2, columnCount: 2), frame: gridFrame)
+
+        let roundedRectOne = UIBezierPath(roundedRect: grid[0, 1]!.insetBy(dx: 5.0, dy: 5.0), cornerRadius: 0.0)
         UIColor.green.setFill()
         roundedRectOne.fill()
-        roundedRectTwo.fill()
-        roundedRectThree.fill()
-        roundedRectFour.fill()
-        roundedRectFive.fill()
+
+        print("\(grid[2]?.height)")
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: 100, y: 100), radius: 50.0, startAngle: 0, endAngle: 90, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.red.setFill()
+        UIColor.black.setStroke()
+        path.stroke()
+        
+//        let path: UIBezierPath = path.copy() as! UIBezierPath
+        path.apply(CGAffineTransform.identity.translatedBy(x: 50, y: 50))
+        path.stroke()
+        path.apply(CGAffineTransform.identity.translatedBy(x: 50, y: 50))
+        
+        path.apply(CGAffineTransform.identity.translatedBy(x: 50, y: 50))
+        path.stroke()
+        
+        let diamondPath = UIBezierPath()
+        diamondPath.move(to: CGPoint.zero)
+        diamondPath.addLine(to: CGPoint(x: 500, y: 500))
+        diamondPath.stroke()
+        
+        
+        
     }
 }
